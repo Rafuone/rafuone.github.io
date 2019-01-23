e2f03376615f79b7f58c51c2489e7d2f9281fc3b
Alexandre Moreira <alexandre.moreira.pro@gmail.com>
2019-01-23 23:45:19 +0100

    Change css to scss, add links, text correction and navbar responsive

--- js/script.js
+++ js/script.js
@@ -20,3 +20,5 @@ $(function () {
         }
     });
 });
+
+ScrollReveal().reveal('.about-content');
---
+++ js/scrollreveal.js
@@ -0,0 +1,38 @@
+ScrollReveal().reveal('.about-resume, .experiences-resume', {
+    duration: 700,
+    distance: '200px',
+      origin:'left',
+      reset: false,
+      useDelay:'always',
+    viewFactor: 3,
+});
+
+
+ScrollReveal().reveal('.landing-description', {
+    duration: 700,
+    distance: '300px',
+      origin:'left',
+      reset: false,
+      useDelay:'always',
+    viewFactor: 3,
+});
+
+ScrollReveal().reveal('.about-content, .experiences-title', {
+    duration: 700,
+    distance: '300px',
+      origin:'top',
+      reset: false,
+      useDelay:'always',
+    viewFactor: 3,
+});
+
+
+ScrollReveal().reveal('.skills-content', {
+    interval:1,
+    duration: 700,
+    distance: '70px',
+      origin:'right',
+      reset: false,
+      useDelay:'always',
+    viewFactor: 0.8,
+});
