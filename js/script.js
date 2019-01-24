b16f1ddfd462b9737d1770591ae1c9fba274756d
Alexandre Moreira <alexandre.moreira.pro@gmail.com>
2019-01-24 01:38:23 +0100

    Add media queries, fix scrollreaveal on mobile

--- js/script.js
+++ js/script.js
@@ -2,23 +2,27 @@ $(function () {
     $(window).scroll(function () {
         let scroll = $(window).scrollTop();
         if (scroll >= 500) {
-            $(".navbar").addClass('smaller');
             $(".cv-btn").addClass('cv-animation');
             $(".mouse-icon").addClass('mouse-exit');
             $(".mouse p").addClass('mouse-p-exit');
+        } else {
+            $(".cv-btn").removeClass('cv-animation');
+            $(".cv-btn").removeClass('cv-animation');
+        }
+    });
+});
 
 
-
+$(function () {
+    $(window).scroll(function () {
+        let scroll = $(window).scrollTop();
+        if (scroll >= 200) {
+            $(".navbar").addClass('smaller');
         } else {
             $(".navbar").removeClass('smaller');
-            $(".cv-btn").removeClass('cv-animation');
-            $(".cv-btn").removeClass('cv-animation');
-
-
-
 
         }
     });
 });
 
-ScrollReveal().reveal('.about-content');
+
--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -1,38 +1,84 @@
-ScrollReveal().reveal('.about-resume, .experiences-resume', {
-    duration: 700,
-    distance: '200px',
-      origin:'left',
-      reset: false,
-      useDelay:'always',
-    viewFactor: 3,
-});
+// if (window.screen.width > 720) {
+    
+//     ScrollReveal().reveal('.about-resume, .experiences-resume', {
+//     duration: 700,
+//     distance: '200px',
+//       origin:'left',
+//       reset: false,
+//       useDelay:'always',
+//     viewFactor: 1,
+// });
 
 
-ScrollReveal().reveal('.landing-description', {
-    duration: 700,
-    distance: '300px',
-      origin:'left',
-      reset: false,
-      useDelay:'always',
-    viewFactor: 3,
-});
+// ScrollReveal().reveal('.landing-description', {
+//     duration: 700,
+//     distance: '300px',
+//       origin:'left',
+//       reset: false,
+//       useDelay:'always',
+//     viewFactor: 1,
+// });
 
-ScrollReveal().reveal('.about-content, .experiences-title', {
-    duration: 700,
-    distance: '300px',
-      origin:'top',
-      reset: false,
-      useDelay:'always',
-    viewFactor: 3,
-});
+// ScrollReveal().reveal('.about-content, .experiences-title', {
+//     duration: 700,
+//     distance: '300px',
+//       origin:'top',
+//       reset: false,
+//       useDelay:'always',
+//     viewFactor: 1,
+// });
 
 
-ScrollReveal().reveal('.skills-content', {
-    interval:1,
-    duration: 700,
-    distance: '70px',
-      origin:'right',
-      reset: false,
-      useDelay:'always',
-    viewFactor: 0.8,
-});
+// ScrollReveal().reveal('.skills-content', {
+//     interval:1,
+//     duration: 700,
+//     distance: '70px',
+//       origin:'right',
+//       reset: false,
+//       useDelay:'always',
+//     viewFactor: 0.8,
+// });
+//   }
+
+
+
+  if (window.matchMedia("(min-width: 720px)").matches) {
+    ScrollReveal().reveal('.about-resume, .experiences-resume', {
+        duration: 700,
+        distance: '200px',
+          origin:'left',
+          reset: false,
+          useDelay:'always',
+        viewFactor: 1,
+    });
+    
+    
+    ScrollReveal().reveal('.landing-description', {
+        duration: 700,
+        distance: '300px',
+          origin:'left',
+          reset: false,
+          useDelay:'always',
+        viewFactor: 1,
+    });
+    
+    ScrollReveal().reveal('.about-content, .experiences-title', {
+        duration: 700,
+        distance: '300px',
+          origin:'top',
+          reset: false,
+          useDelay:'always',
+        viewFactor: 1,
+    });
+    
+    
+    ScrollReveal().reveal('.skills-content', {
+        interval:1,
+        duration: 700,
+        distance: '70px',
+          origin:'right',
+          reset: false,
+          useDelay:'always',
+        viewFactor: 0.8,
+    });
+  }
